53293838664c51295fb275a7bb3c4fc8695eb267
Grant Russell <grant.s.russ@gmail.com>
2019-05-01 02:01:18 -0600

    Mock database response (#1)
    * mock out database request and send dummy json
    
    * remove console.log

---
+++ database.js
@@ -0,0 +1,24 @@
+function getDatabaseData(){
+    const fakeDatabaseData = {
+        helps: [
+            {
+                title: 'Need to move a couch',
+                address: '123 Beaker St, Logan UT',
+            },
+            {
+                title: 'Get a cat out of the tree',
+                address: '124 N 400 E, Logan UT',
+            },
+            {
+                title: 'Need help cutting toe nails',
+                address: '10 N Main St, Logan UT',
+            }
+        ]
+    };
+
+    return fakeDatabaseData;
+}
+
+module.exports = {
+    getDatabaseData
+};  
