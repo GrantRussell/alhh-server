f1eb1ccb32533a17e665868a6fb292bac6e64d0e
Grant Russell <grant.s.russ@gmail.com>
2019-05-01 01:56:28 -0600

    mock out database request and send dummy json

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const express = require('express');
+const database = require('./database');
 const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello World, I am A Little Help Here!');
+    res.status(200).send('Use endpoint \"getHelps\" to retrieve data.');
 });
 
+app.get('/getHelps', (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    const databaseResponse = database.getDatabaseData();
+    console.log(databaseResponse);
+    res.status(200).send(JSON.stringify(databaseResponse));
+});
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}...`);
